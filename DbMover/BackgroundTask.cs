bce505b6396e4128c6fd909208d3e28ac12ccc30
Aragorn <aragorn@gmail.com>
2022-10-22 03:11:11 +0800

    check

---
+++ DbMover/BackgroundTask.cs
@@ -0,0 +1,180 @@
+﻿using Serilog;
+using System.Data.SqlClient;
+
+namespace DbMover;
+
+internal class BackgroundTask
+{
+    private Task? _timerTask;
+    private readonly PeriodicTimer _timer;
+    private readonly CancellationTokenSource _cts = new();
+    private readonly string localConnectionStr;
+    private readonly string remoteConnectionStr;
+    public BackgroundTask(string local, string remote, TimeSpan inverval)
+    {
+        localConnectionStr = local;
+        remoteConnectionStr = remote;
+        _timer = new PeriodicTimer(inverval);
+    }
+    async public     Task
+Start()
+    {
+        await ActualWork();
+        _timerTask = DoWorkAsync();
+    }
+
+    async Task<Dictionary<DateTime, float>> GetGenerating()
+    {
+        string queryStr = @"SELECT TOP 1152 
+        [PointSliceID]
+        ,[UTCDateTime]
+        ,[ActualValue]
+        FROM [JCIHistorianDB].[dbo].[tblActualValueFloat]
+        Where PointSliceID in (624, 626, 628, 630, 632, 634, 636, 638)
+        Order by UTCDateTime desc";
+
+        using (SqlConnection connection = new SqlConnection(remoteConnectionStr))
+        {
+            SqlCommand command = new SqlCommand(queryStr, connection);
+            command.Connection.Open();
+            Dictionary<int, List<(DateTime, float)>> pointTimeValueMap = new();
+            var reader = await command.ExecuteReaderAsync();
+            while (reader.Read())
+            {
+                int point = (int)reader["PointSliceID"];
+                DateTime dt = (DateTime)reader["UTCDateTime"];
+                float value = (float)reader["ActualValue"];
+                if (!pointTimeValueMap.ContainsKey(point))
+                    pointTimeValueMap[point] = new();
+
+                pointTimeValueMap[point].Add((dt, value));
+            }
+            IEnumerable<List<(DateTime, float)>> pointGeneratingEnumerator = pointTimeValueMap.Select(kvp =>
+            {
+                var totalList = kvp.Value;
+                List<(DateTime, float)> generatingList = new();
+                while (totalList.Count >= 2)
+                {
+                    var end = totalList[0];
+                    var start = totalList[1];
+                    float generatedKWHour = end.Item2 - end.Item2;
+                    TimeSpan ts = start.Item1 - start.Item1;
+                    float generatedKW = (float)(generatedKWHour / ts.TotalMinutes * 60);
+                    generatingList.Add((start.Item1, generatedKW));
+                    totalList.RemoveAt(0);
+                }
+                return generatingList;
+            });
+            Dictionary<DateTime, float> totalGeneratingMap = new();
+            foreach (List<(DateTime, float)> generatingList in pointGeneratingEnumerator)
+            {
+                foreach (var generating in generatingList)
+                {
+                    if (!totalGeneratingMap.ContainsKey(generating.Item1))
+                        totalGeneratingMap[generating.Item1] = 0;
+
+                    totalGeneratingMap[generating.Item1] += generating.Item2;
+                }
+            }
+            return totalGeneratingMap;
+        }
+    }
+
+    async Task<Dictionary<DateTime, float>> GetConsuming()
+    {
+        string queryStr = @"SELECT TOP 144 
+            [PointSliceID],[UTCDateTime],[ActualValue]
+            FROM [JCIHistorianDB].[dbo].[tblActualValueFloat]
+            Where PointSliceID = 239
+            Order by UTCDateTime desc";
+
+        using (SqlConnection connection = new SqlConnection(remoteConnectionStr))
+        {
+            SqlCommand command = new SqlCommand(queryStr, connection);
+            command.Connection.Open();
+            Dictionary<DateTime, float> consumingMap = new Dictionary<DateTime, float>();
+            var reader = await command.ExecuteReaderAsync();
+            while (reader.Read())
+            {
+                DateTime dt = (DateTime)reader["UTCDateTime"];
+                float value = (float)reader["ActualValue"];
+                consumingMap.Add(dt.AddHours(8), value);
+            }
+            return consumingMap;
+        }
+    }
+
+    async Task<int> UpdateMinRecord(DateTime dt, float generating, float consuming)
+    {
+        string queryStr = @"INSERT INTO [dbo].[MinRecord]
+           ([MonitorID]
+           ,[DateTime]
+           ,[generating]
+           ,[storing]
+           ,[consuming])
+     VALUES
+           (3
+           ,@dt
+           ,@generating
+           ,null
+           ,@consuming)";
+
+        using (SqlConnection connection = new SqlConnection(localConnectionStr))
+        {
+            SqlCommand command = new SqlCommand(queryStr, connection);
+            command.Parameters.AddWithValue("generating", generating);
+            command.Parameters.AddWithValue("consuming", consuming);
+            command.Parameters.AddWithValue("dt", dt);
+            command.Connection.Open();
+            return await command.ExecuteNonQueryAsync();
+        }
+    }
+
+    private async Task ActualWork()
+    {
+        try
+        {
+            var generatingMap = await GetGenerating();
+            var consumingMap = await GetConsuming();
+            List<(DateTime, float, float)> updateList = new();
+            foreach(var dt in generatingMap.Keys)
+            {
+                if (!consumingMap.ContainsKey(dt))
+                    continue;
+
+                updateList.Add((dt, generatingMap[dt], consumingMap[dt]));
+            }
+            updateList.Sort((a, b) => a.Item1.CompareTo(b.Item1));
+
+        }catch(Exception ex)
+        {
+            Log.Error(ex, "failed");
+        }
+        return;
+    }
+
+    private async Task DoWorkAsync()
+    {
+        try
+        {
+            while (await _timer.WaitForNextTickAsync(_cts.Token))
+            {
+                Log.Information("Start doing task");
+                await ActualWork();
+            }
+        }
+        catch (OperationCanceledException)
+        {
+
+        }
+    }
+    public async Task StopAsync()
+    {
+        if (_timerTask is null) return;
+
+        _cts.Cancel();
+        await _timerTask;
+        _cts.Dispose();
+        Console.WriteLine("Task was canceled");
+    }
+}
--- DbMover/Program.cs
+++ DbMover/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DbMover;
 using Serilog;
 using System.Data.SqlClient;
 using System.Diagnostics.CodeAnalysis;
@@ -19,133 +20,14 @@ Console.WriteLine(remoteBuilder.ConnectionString);
 
 Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Information()                
+                .WriteTo.Console()
                 .WriteTo.File(".\\log\\DbMover.log", rollingInterval: RollingInterval.Day)
                 .CreateLogger();
 
-async Task<float?> GetGenerating()
-{
-    string queryStr = @"SELECT TOP 1
-            [PointSliceID]
-            ,[UTCDateTime]
-            ,[OtherValue]
-            FROM [JCIHistorianDB].[dbo].[tblOtherValueFloat]
-            Where PointSliceID in (627, 629, 631, 633, 635, 637, 639) and UTCDateTime>= @start and UTCDateTime <@end
-            Order by UTCDateTime desc";
-
-    using (SqlConnection connection = new SqlConnection(remoteBuilder.ConnectionString))
-    {
-        SqlCommand command = new SqlCommand(queryStr, connection);
-        command.Parameters.AddWithValue("start", DateTime.Now.AddHours(-1));
-        command.Parameters.AddWithValue("end", DateTime.Now);
-        command.Connection.Open();
-        var reader = await command.ExecuteReaderAsync();
-        while (reader.Read())
-        {
-            if (reader["OtherValue"] != DBNull.Value)
-                return (float)reader["OtherValue"];
-
-            return null;
-        }
-        return null;
-    }
-}
-
-async Task<float?> GetConsuming()
-{
-    string queryStr = @"SELECT TOP 1
-            [PointSliceID]
-            ,[UTCDateTime]
-            ,[OtherValue]
-            FROM [JCIHistorianDB].[dbo].[tblOtherValueFloat]
-            Where PointSliceID = 239 and UTCDateTime>= @start and UTCDateTime <@end
-            Order by UTCDateTime desc";
-
-    using (SqlConnection connection = new SqlConnection(remoteBuilder.ConnectionString))
-    {
-        SqlCommand command = new SqlCommand(queryStr, connection);
-        command.Parameters.AddWithValue("start", DateTime.Now.AddHours(-1));
-        command.Parameters.AddWithValue("end", DateTime.Now);
-        command.Connection.Open();
-        var reader = await command.ExecuteReaderAsync();
-        while (reader.Read())
-        {
-            if (reader["OtherValue"] != DBNull.Value)
-                return (float)reader["OtherValue"];
-
-            return null;
-        }
-        return null;
-    }
-}
-
-async Task<int> UpdateMinRecord(float? generating, float? consuming)
-{
-    string queryStr = @"INSERT INTO [dbo].[MinRecord]
-           ([MonitorID]
-           ,[DateTime]
-           ,[generating]
-           ,[storing]
-           ,[consuming])
-     VALUES
-           (3
-           ,@now
-           ,@generating
-           ,null
-           ,@consuming)";
-
-    using (SqlConnection connection = new SqlConnection(localBuilder.ConnectionString))
-    {
-        SqlCommand command = new SqlCommand(queryStr, connection);
-        command.Parameters.AddWithValue("generating", generating.GetValueOrDefault(0));       
-        command.Parameters.AddWithValue("consuming", consuming.GetValueOrDefault(0));
-        var now = DateTime.Now;
-        command.Parameters.AddWithValue("now", now.AddSeconds(-now.Second).AddMilliseconds(-now.Millisecond));
-        command.Connection.Open();
-        return await command.ExecuteNonQueryAsync();        
-    }
-}
-
-async void GrabMinData()
-{
-    try
-    {
-        var generating = await GetGenerating();
-        var consuming = await GetConsuming();
-        Console.WriteLine($"{generating} - {consuming}");
-        int affected = await UpdateMinRecord(generating, consuming);
-        Console.WriteLine($"Insert {affected} min records");
-    }
-    catch (Exception ex)
-    {
-        Log.Error(ex, "move failed...");
-    }
-}
-
-System.Timers.Timer minTimer;
-
-void SetupMinTimer()
-{
-    DateTime now = DateTime.Now;
-    TimeSpan ts = DateTime.Now.AddMinutes(1).AddSeconds(-now.Second + 30).AddMilliseconds(-now.Millisecond) - now;
-    Log.Information($"set up 1 min timer");
-    minTimer = new System.Timers.Timer(ts.TotalMilliseconds);
-    minTimer.Elapsed += (sender, e) =>
-    {
-        DateTime currnt = DateTime.Now;
-        TimeSpan nextTimeSpan = currnt.AddMinutes(1).AddMilliseconds(-currnt.Millisecond) - DateTime.Now;
-        minTimer.Interval = nextTimeSpan.TotalMilliseconds;
-        DateTime dtNow = DateTime.Now;
-        DateTime adjustedNow = DateTime.Now.Date.AddMinutes((int)dtNow.TimeOfDay.TotalMinutes).AddMinutes(-2);
-        GrabMinData();
-
-        minTimer.Start();
-    };
-    minTimer.Start();
-}
-
-SetupMinTimer();
-
-Console.WriteLine("Type exit to exit");
+Console.WriteLine("Start grab exhibition db");
+var task = new BackgroundTask(localBuilder.ConnectionString, remoteBuilder.ConnectionString, TimeSpan.FromHours(1));
+await task.Start();
 while (Console.ReadLine() != "exit");
+await task.StopAsync();
 
-minTimer.Stop();
+
